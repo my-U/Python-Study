adbd4e541b9b7c6bc2687ac55ce410c1d8c4ba06
my-U <myouth_u@naver.com>
2021-10-28 20:40:40 +0900

    문자열 처리함수

--- PythonWorkspace/practice.py
+++ PythonWorkspace/practice.py
@@ -1,10 +1,18 @@
-jumin = "990120-1234567"
+python = "Python is Amazing"
 
-print("성별 : " + jumin[7]) # 인덱스 7번째 값
-print("년도 : " + jumin[0:2]) # 인덱스 0부터 2직전까지
-print("월 : " + jumin[2:4]) # 인덱스 2부터 4직전까지
-print("일 : " + jumin[4:6]) # 인덱스 4부터 6직전까지
+print(python.lower()); # 모든 문자 소문자로 출력
+print(python.upper()); # 모든 문자 대문자로 출력
+print(python[0].isupper()); # 인덱스 0번째 문자가 대문자인지
+print(len(python)) # python 문자열의 길이
+print(python.replace("Python", "Java")); # 문자열 중 Python을 Java로 변경
 
-print("생년월일 : " + jumin[:6]) # 인덱스 처음부터 6직전까지
-print("뒷번호 7자리 : " + jumin[7:]) # 인덱스 7부터 끝까지
-print("뒷번호 7자리(뒤에서부터) : " + jumin[-7:]) # 가장 끝번호는 인덱스 0의 앞이기 때문에 인덱스 -1이다
+index = python.index("n") # 문자 n의 인덱스 번호 출력
+print(index) # index = 5
+index = python.index("n", index + 1) # 위의 index의 값 + 1 부터 즉, 인덱스 6부터 문자 n을 찾음
+print(index) # index = 15
+
+print(python.find("n")) # 문자 n의 인덱스 번호 출력
+print(python.find("Java")) # 문자열에 Java가 존재하지 않아 -1 출력 후 다음 코드 실행
+print(python.index("Java")) # 문자열에 Java가 존재하지 않아 오류 출력 후 실행 중단
+
+print(python.count("n")) # 문자 n이 문자열에서 몇 번 존재하는지 세줌
