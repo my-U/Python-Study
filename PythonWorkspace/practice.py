3fd07cabafaae7bc9cc1f260553be87875765bf8
my-U <myouth_u@naver.com>
2021-10-28 18:25:47 +0900

    문자열

--- PythonWorkspace/practice.py
+++ PythonWorkspace/practice.py
@@ -1,10 +1,9 @@
-from random import *  # random 라이브러리에 들어있는 모든 것을 사용
-
-print(random())  # 0.0에서 1.0 미만의 임의의 값을 생성
-print(random() * 10) # 0.0에서 10.0 미만의 임의의 값을 생성
-print(int(random() * 10)) # 0에서 10 미만의 임의의 정수값 생성
-print(int(random() * 10) + 1) # 1에서 11미만의 임의의 정수값 생성
-
-print(int(random() * 45) + 1) # 1에서 46 미만의 임의의 정수값 생성
-print(randrange(1, 46)) # 1에서 46 미만의 임의의 값 생성
-print(randint(1, 45)) # 1에서 45 이하의 임의의 값 생성
+sentence = "나는 소녀입니다"
+print(sentence)
+sentence2 = "파이썬은 쉬워요"
+print(sentence2)
+sentence3 = """
+나는 소녀이고,
+파이썬은 쉬워요
+""" # 여러줄 출력 가능
+print(sentence3)
